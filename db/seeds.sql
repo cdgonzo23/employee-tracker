94c84f491e5d52efd04fa5533351efd9bc5cc653
Christian Gonzales <124095890+cdgonzo23@users.noreply.github.com>
2023-07-15 11:42:01 -0700

    added insert employee, role, and department capability

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,7 +4,7 @@ INSERT INTO departments (name)
     VALUES ('Human Resources'), ('Accounting');
 
 INSERT INTO roles (title, salary) 
-    VALUES ('HR Manager', 60000), ('Accountant', 70000);
+    VALUES ('HR Manager', 60000), ('Accountant', 80000);
 
 INSERT INTO employees (first_name, last_name) 
     VALUES ('John', 'Doe'), ('Jane', 'Wilson');
--- index.js
+++ index.js
@@ -12,6 +12,36 @@ const getAll = (tableName) => {
     });
 }
 
+const insertEmployee = (data) => {
+    db.query('INSERT INTO employees SET ?', data, (err, result) => {
+        if (err) {
+            return console.error(err);
+        }
+        console.log('Added Employee');
+        init();
+    });
+};
+
+const insertRole = (data) => {
+    db.query('INSERT INTO roles SET ?', data, (err, result) => {
+        if (err) {
+            return console.error(err);
+        }
+        console.log('Added Role');
+        init();
+    });
+};
+
+const insertDepartment = (data) => {
+    db.query('INSERT INTO departments SET ?', data, (err, result) => {
+        if (err) {
+            return console.error(err);
+        }
+        console.log('Added Role');
+        init();
+    });
+};
+
 const handleAction = ({ action }) => {
     console.log(`ACTION ${action}`);
     switch(action) {
@@ -27,6 +57,41 @@ const handleAction = ({ action }) => {
             getAll('roles');
             break;
         }
+        case 'Add an Employee': {
+            prompt([
+                {
+                    message: 'Enter First Name',
+                    name: 'first_name',
+                },
+                {
+                    message: 'Enter Last Name',
+                    name: 'last_name'
+                },
+            ]).then(insertEmployee);
+            break;
+        }
+        case 'Add a Role': {
+            prompt([
+                {
+                    message: 'What is the title of the role?',
+                    name: 'title',
+                },
+                {
+                    message: 'What is the salary of the role?',
+                    name: 'salary'
+                },
+            ]).then(insertRole);
+            break;
+        }
+        case 'Add a Department': {
+            prompt([
+                {
+                    message: 'What is the name of the department?',
+                    name: 'name',
+                },
+            ]).then(insertDepartment);
+            break;
+        }
         default: {
             process.exit();
         }
@@ -40,8 +105,12 @@ const init = () => {
         name: 'action',
         choices: [
             'View All Employees',
-            'View All Departments',
+            'Add an Employee',
+            // 'Update an Employee Role',
             'View All Roles',
+            'Add a Role',
+            'View All Departments',
+            'Add a Department',
             'Exit',
         ]
     }).then(handleAction);
